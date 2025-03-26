6d9ae3724f9235f82f399aaa05fde39fc9c68403
Argi <argigjini@gmail.com>
2025-03-26 12:16:02 +0000

    Created_Ising_2d_Model_Header

---
+++ IsingModel2d.h
@@ -0,0 +1,23 @@
+#ifndef ISINGMODEL2D_H
+#define ISINGMODEL2D_H
+
+#include "IsingModel1D.h"
+#include <vector>
+
+class IsingModel2D : public IsingModel1D {
+public:
+    IsingModel2D(int size, double beta);
+    void simulate(int steps, int burn_in, int sample_interval);
+
+private:
+    int size;
+    std::vector<std::vector<int>> spins;
+
+    void initialize_spins();
+    double energy_change(int x, int y);
+    void monte_carlo_step();
+    double total_energy();
+    double magnetization();
+};
+
+#endif
